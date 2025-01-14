26f16dd709441f00a07704af18d833190fc39e1d
Abdullah1186 <abdullahalfekaiki@gmail.com>
2025-01-14 11:31:55 +0000

    incorperating the fragment step the GeoLDM use

--- scripts /rdkitfunctions_remake.py
+++ scripts /rdkitfunctions_remake.py
@@ -12,16 +12,16 @@ def rdkitfun(dataset,model):
 
 
     if dataset == 'QM9':
-        with open("/root/GeoLdm/databases/qm9_smiles.json", "r") as file:
+        with open("/root/GeoLDM/databases/qm9_smiles.json", "r") as file:
             DBsmiles= json.load(file)
         
         if model =='Gschnet':
-            with open("/root/GeoLdm/databases/48h_smiles.json", "r") as file:
+            with open("/root/GeoLDM/databases/48h_smiles.json", "r") as file:
                 smiles_list = json.load(file)
             
                 
         elif model =='GeoLDM':
-            with open("/root/GeoLdm/databases/GeoLDM_smiles.json", "r") as file:
+            with open("/root/GeoLDM/databases/GeoLDM_smiles.json", "r") as file:
                 smiles_list = json.load(file)
 
         else: 
@@ -30,11 +30,11 @@ def rdkitfun(dataset,model):
             
 
     if dataset == 'OE62':
-        with open("/root/GeoLdm/databases/OE62_full_smiles.json", "r") as file:
+        with open("/root/GeoLDM/databases/OE62_full_smiles.json", "r") as file:
                 DBsmiles= json.load(file)
                 
         if model == 'Gschnet':
-            with open("/root/GeoLdm/databases/0E62_20k_1.7_2.3_smiles.json", "r") as file:
+            with open("/root/GeoLDM/databases/0E62_20k_1.7_2.3_smiles.json", "r") as file:
                 smiles_list = json.load(file) 
 
             
@@ -44,29 +44,27 @@ def rdkitfun(dataset,model):
             smiles_list = None
 
 
-    def is_valid_smiles(smiles):
-        """
-        Check if a SMILES string is valid.
-        
-        Parameters:
-            smiles (str): The SMILES string to validate.
-        
-        Returns:
-            bool: True if valid, False otherwise.
-        """
-        mol = Chem.MolFromSmiles(smiles)
-        return mol is not None
 
-    valid=[]
-    for i in smiles_list: 
-        if is_valid_smiles(i) == True:
-            valid.append(i)
+    def compute_validity(smiles):
+            
+        valid = []
 
-    validity = len(valid)/len(smiles_list)
+        for smile in smiles:
+            mol = Chem.MolFromSmiles(smile)
+            if smiles and mol is not None:
+                mol_frags = Chem.rdmolops.GetMolFrags(mol, asMols=True)
+                largest_mol = max(mol_frags, default=mol, key=lambda m: m.GetNumAtoms())
+                new_smiles = Chem.MolToSmiles(largest_mol)
+                valid.append(new_smiles)
+
+        return valid, len(valid) / len(smiles)
+
+    valid, validity = compute_validity(smiles_list)
+   
 
     def compute_uniqueness(valid):
-            """ valid: list of SMILES strings."""
-            return len(set(valid)) / len(valid)
+        """ valid: list of SMILES strings."""
+        return len(set(valid)) / len(valid)
 
     unique= set(valid)
     uniqueness=compute_uniqueness(valid)
@@ -81,6 +79,7 @@ def rdkitfun(dataset,model):
                 novel.append(smiles)
                 num_novel += 1
         return num_novel / len(unique)
+    
     novelty=compute_novelty(unique)
 
 
